3043601e38b4157a5b00f14c4b1bc396b67ed3bf
gloria <nyagagakiigloria@gmail.com>
2024-02-08 21:18:12 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main() {
+    var name: String = "Talia Mukami"
+    println (name)
+
+    var age: Byte = 19
+    println (19)
+
+    var phoneNumber: String = "(+254)712345678"
+    println (phoneNumber)
+
+    var weight: Float = 58.20F
+     println (weight)
+
+    var citizen : String = "Kenyan"
+    println (citizen)
+
+    var Kenyan: Boolean = true
+    println (Kenyan)
+
+}
